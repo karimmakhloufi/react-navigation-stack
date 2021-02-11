3ef6b5713f110aad4b160bc76abf41133a9b6aa1
Karim Makhloufi <maclouf@gmail.com>
2021-02-11 18:46:45 +0100

    TextInput and button

--- App.js
+++ App.js
@@ -1,63 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
     <ScrollView>
-      <View style={{ backgroundColor: "red" }}>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
-        <Text>No style</Text>
+      <View style={{ backgroundColor: "lightgrey" }}>
+        <TextInput></TextInput>
+        <Button title="Push me"></Button>
       </View>
     </ScrollView>
   );
