ecc219b9e26548f379ff8db340c087769316ee46
Karim Makhloufi <maclouf@gmail.com>
2021-02-12 16:51:26 +0100

    Displaying Images from API

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,19 +7,33 @@ import {
   ScrollView,
   TextInput,
   Button,
+  Image,
 } from "react-native";
+
+import axios from "axios";
 
 export default function App() {
   const [inputValue, setInputValue] = useState("defaultValue");
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const result = await axios("https://randomuser.me/api/?results=50");
+      setUsers(result.data.results);
+    })();
+  }, []);
   return (
     <ScrollView style={{ paddingTop: 50 }}>
-      <View style={{ backgroundColor: "lightgrey" }}>
-        <TextInput
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        ></TextInput>
-        <Button title="Push me" onPress={() => alert(inputValue)}></Button>
-      </View>
+      {users.length > 0
+        ? users.map((el, index) => (
+            <View key={index}>
+              <Text>{el.email}</Text>
+              <Image
+                style={{ width: 100, height: 100 }}
+                source={{ uri: el.picture.large }}
+              />
+            </View>
+          ))
+        : null}
     </ScrollView>
   );
 }
